b262ad4044838660b19fac119744109a724650ab
amandabarros94 <mandy.bjj1@gmail.com>
2022-09-08 20:36:33 -0300

    Feat- [req.11 ]- comando retorna o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken

---
+++ challenges/desafio11.js
@@ -0,0 +1,16 @@
+db.produtos.find(
+    { $and: [{
+        nome: { 
+            $not: { 
+                $in: ["Big Mac", "McChicken"],
+            }, 
+        }, 
+    }], 
+    },
+    {
+      nome: 1,
+      curtidas: 1,
+      vendidos: 1,
+      _id: 0,
+    },
+);
