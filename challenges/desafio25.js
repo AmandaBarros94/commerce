015f6a90e0f46b2a4f5600bef6326b122b2035b8
amandabarros94 <mandy.bjj1@gmail.com>
2022-09-08 22:25:41 -0300

    Feat- [req.25 ]- comando adciona o valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40

---
+++ challenges/desafio25.js
@@ -0,0 +1,19 @@
+db.produtos.updateOne(
+    {
+        valoresNutricionais: {
+            $elemMatch: {
+                tipo: "sódio", percentual: { $gte: 40 },
+            },
+        },
+    },
+    {
+        $push: {
+            tags: "muito sódio",
+        },
+    },
+);
+
+db.produtos.find(
+    {},
+    { nome: 1, tags: 1, _id: 0 },
+);
